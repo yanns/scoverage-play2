55b37161bb8d02903be54771604d96eb273b3f29
Yann Simon <yann.simon.fr@gmail.com>
2014-06-19 10:41:08 +0200

    fix scoverage with assets

--- project/ScoverageIntegration.scala
+++ project/ScoverageIntegration.scala
@@ -1,9 +1,19 @@
+import sbt._
+import sbt.Keys._
+import com.typesafe.sbt.web.Import.TestAssets
+import com.typesafe.sbt.web.Import.WebKeys.assets
 import scoverage.ScoverageSbtPlugin._
 
 object ScoverageIntegration {
 
   val settings = instrumentSettings ++ Seq(
     ScoverageKeys.excludedPackages in ScoverageCompile :=
-      """views\..*;controllers\..*Reverse.*;controllers\..*javascript.*;controllers\..*ref.*;Routes.*"""
+      """views\..*;controllers\..*Reverse.*;controllers\..*javascript.*;controllers\..*ref.*;Routes.*""",
+
+    fullClasspath in ScoverageTest ++= {
+      val testAssetDirs = ((assets in TestAssets) ?).all(ScopeFilter(inDependencies(ThisProject))).value.flatten
+      testAssetDirs.map(dir => Attributed.blank(dir.getParentFile))
+    }
+
   )
 }
